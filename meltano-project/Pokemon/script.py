0d2d7f095460fdf47e9384d662239427211c7390
root <root@ip-172-31-43-149.ap-south-1.compute.internal>
2023-06-26 08:07:13 +0000

    new

--- meltano-project/Pokemon/script.py
+++ meltano-project/Pokemon/script.py
@@ -1,7 +1,22 @@
 import os
+import pandas as pd
+import csv
+
 pokemon=['pikachu','charizard' , 'snorlax' , 'squirtle' ]
 for i in pokemon:
  cmd='meltano config tap-pokeapi set airbyte_config pokemon_name ' + i
  cmd2='meltano run tap-pokeapi target-csv'
  os.system(cmd)
- os.system(cmd2) 
+ os.system(cmd2)
+
+f_list=[]
+for dirpath, dirs, files in os.walk('/root/meltano/meltano-project/Pokemon/New-output'):
+ for filename in files:
+  fname = os.path.join(dirpath,filename)
+  if fname.endswith('.csv'):
+   f_list.append(fname)
+
+df_csv_concat = pd.concat([pd.read_csv(file) for file in f_list ], ignore_index=True)
+
+df_csv_concat.to_csv('pokemon_list.csv')
+
